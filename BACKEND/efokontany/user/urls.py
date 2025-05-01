703840f8729daf7e6b03cd69b751b252420a2565
Mikle <miklenomeny@gmail.com>
2025-05-01 08:23:10 +0300

    first commit

--- BACKEND/efokontany/user/urls.py
+++ BACKEND/efokontany/user/urls.py
@@ -4,5 +4,5 @@ from .views import UserView, LoginView
 urlpatterns = [
     path('user/', UserView.as_view(), name='user'),
     path('user/<int:pk>/', UserView.as_view(), name='user_detail'),
-    path('user/login/', LoginView.as_view(), name='login')
+    path('user/login/', LoginView.as_view(), name='login'),
 ]
